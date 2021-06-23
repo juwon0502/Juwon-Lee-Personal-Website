9b9a9ff4d0058ad71831c1b347e622c9d6574dca
juwon0502 <juwon.lee@utexas.edu>
2021-06-23 16:18:07 -0500

    Add project files.

---
+++ Lee_Juwon_HW1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lee_Juwon_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult personal()
+        {
+            return View();
+        }
+        public IActionResult cooking()
+        {
+            return View();
+        }
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult resume()
+        {
+            string path = _environment.WebRootPath + "/files/Juwon Lee Resume.pdf";            
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var fsResult = new FileStreamResult(stream, "application/pdf");
+            return fsResult;
+        }
+    }
+}
---
+++ Lee_Juwon_HW1/Models/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lee_Juwon_HW1.Models
+{
+    public class Repository
+    {
+    }
+}
